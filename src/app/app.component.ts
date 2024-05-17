2f74ab8c6661284f95086873198eec7c59a1c6ff
lubinga <pfatsch@icloud.com>
2024-05-17 18:11:50 +0200

    data formatting with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Course } from "./model/course";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  courses = [...COURSES];
+  courses:Course[] = [...COURSES];
+
+  startDate = new Date();
+  title = this.courses[0].description;
+  price = 9.9915648646;
+  rate = 0.67;
+  course = COURSES[0];
 
   onCourseSelected(course: Course) {
 
